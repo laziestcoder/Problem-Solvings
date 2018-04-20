2f1ac09be3ed4303bebffb5509888268bf481bc1
Towfiqul Islam <towfiq.106@gmail.com>
2018-04-21 01:11:25 +0600

    Add files via upload

---
+++ theory-based-problems/2.1 fibonacci_number_recursive.c
@@ -0,0 +1,31 @@
+/************************************************************************/
+/*	Name: Towfiqul Islam 												*/
+/*	uva id: 448714 														*/
+/*	email id: towfiq.106@gmail.com										*/
+/*	institute: IIUC														*/
+/*	address: Chittagong, Bangladesh										*/
+/*																		*/
+/************************************************************************/
+
+/*
+fibonacci number recursive
+*/
+
+#include<stdio.h>
+
+int fib(int n)
+{
+    if(n==1||n==2)
+        return 1;
+    else
+        return fib(n-1)+fib(n-2);
+}
+
+int main()
+{
+    int n;
+    printf("which fib number : ");
+    scanf("%d",&n);
+    printf("fib[%d] = %d",n,fib(n));
+    return 0;
+}
