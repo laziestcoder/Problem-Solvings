c25008932fa84e6826b0355212f8d1a1682bb378
Towfiqul Islam <towfiq.106@gmail.com>
2018-04-21 00:58:27 +0600

    Add files via upload

---
+++ CMD CODES/fbizzd.bat
@@ -0,0 +1,2 @@
+@echo off
+ping www.facebook.com/renesmy -t -| 99999
---
+++ CMD CODES/mat.bat
@@ -0,0 +1,5 @@
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto top
