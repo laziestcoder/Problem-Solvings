8ba96ff98bb7181494ff7eec195a9c21e5468436
laziestcoder <towfiq.106@gmail.com>
2018-10-25 08:14:43 +0600

    this is template

--- mytemp.cpp
+++ mytemp.cpp
@@ -371,6 +371,38 @@ ll modpow(ll b, ll n, ll m) {  //b=base, n=power, m=mod
 ***/
 
 
+/***
+const int MAX = 1000;
+
+// Create an array for memoization
+int f[MAX] = {0};
+
+// Returns n'th fibonacci number using table f[]
+int fib(int n)
+{
+	// Base cases
+	if (n == 0)
+		return 0;
+	if (n == 1 || n == 2)
+		return (f[n] = 1);
+
+	// If fib(n) is already computed
+	if (f[n])
+		return f[n];
+
+	int k = (n & 1)? (n+1)/2 : n/2;
+
+	// Applyting above formula [Note value n&1 is 1
+	// if n is odd, else 0.
+	f[n] = (n & 1)? (fib(k)*fib(k) + fib(k-1)*fib(k-1))
+		: (2*fib(k-1) + fib(k))*fib(k);
+
+	return f[n];
+}
+
+***/
+
+
 /************************** Functions Ends Here ***************************/
 
 
